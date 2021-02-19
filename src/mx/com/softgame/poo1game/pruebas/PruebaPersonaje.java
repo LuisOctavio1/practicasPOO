d1996e621b54e92da5ce6fafc8c1159384e44605
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-02-19 07:24:11 -0800

    Cambio 9:23
    Creación de los objetos

--- src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -2,9 +2,16 @@ package mx.com.softgame.poo1game.pruebas;
 import mx.com.softgame.poo1game.personajes.Personaje;
 public class PruebaPersonaje {
     public static void main(String[]args){
-        
         Personaje per01 = new Personaje("Aline");
         per01.setEdad(200)
+        Personaje per02 = new Personaje("Erick");
+        per01.setEdad(10)
+        Personaje per03 = new Personaje("Alan");
+        per01.setEdad(1)
+        Personaje per04 = new Personaje("Lesly");
+        per01.setEdad(-10)
+        Personaje per05 = new Personaje("Brian");
+        per01.setEdad(500)
 
 
     }
