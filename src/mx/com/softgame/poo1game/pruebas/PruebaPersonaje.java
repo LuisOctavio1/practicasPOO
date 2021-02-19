3031d2367ca39f5b291e814a3fb6e66294aa50b5
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-02-19 07:33:20 -0800

    Cambios 9:33
    Se crean las nuevas instancias

--- src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -19,6 +19,12 @@ public class PruebaPersonaje {
         System.out.println(per04.getDetalle());
         System.out.println(per05.getDetalle());
 
+        Personaje per06 = per05;
+        Personaje per07 = per04;
+        Personaje per08 = per03;
+        Personaje per09 = per02;
+        Personaje per10 = per01;
+
 
 
     }
