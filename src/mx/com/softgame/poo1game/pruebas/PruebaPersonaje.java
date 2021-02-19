57d19a92e3b325f9ae6f7d552c8fae4292452c46
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-02-19 07:31:26 -0800

    Cambio 9:28
    Se imprime por el metodo getDetalle

--- src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -3,15 +3,22 @@ import mx.com.softgame.poo1game.personajes.Personaje;
 public class PruebaPersonaje {
     public static void main(String[]args){
         Personaje per01 = new Personaje("Aline");
-        per01.setEdad(200)
+        per01.setEdad(200);
         Personaje per02 = new Personaje("Erick");
-        per01.setEdad(10)
+        per01.setEdad(10);
         Personaje per03 = new Personaje("Alan");
-        per01.setEdad(1)
+        per01.setEdad(1);
         Personaje per04 = new Personaje("Lesly");
-        per01.setEdad(-10)
+        per01.setEdad(-10);
         Personaje per05 = new Personaje("Brian");
-        per01.setEdad(500)
+        per01.setEdad(500);
+
+        System.out.println(per01.getDetalle());
+        System.out.println(per02.getDetalle());
+        System.out.println(per03.getDetalle());
+        System.out.println(per04.getDetalle());
+        System.out.println(per05.getDetalle());
+
 
 
     }
