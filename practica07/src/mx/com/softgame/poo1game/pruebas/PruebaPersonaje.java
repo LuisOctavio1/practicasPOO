b150472bd66831012ccfbe8b3d0274f042ca85cf
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-02-26 06:13:42 -0800

    cambio 8:13
    se inicia el ciclo for

--- practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -3,15 +3,10 @@ import mx.com.softgame.poo1game.personajes.Personaje;
 public class PruebaPersonaje {
     public static void main(String[]args){
         Personaje per01 = new Personaje("Aline");
-        per01.setEdad(200);
         Personaje per02 = new Personaje("Erick");
-        per02.setEdad(10);
         Personaje per03 = new Personaje("Alan");
-        per03.setEdad(1);
         Personaje per04 = new Personaje("Lesly");
-        per04.setEdad(-10);
         Personaje per05 = new Personaje("Brian");
-        per05.setEdad(500);
 
 
 
