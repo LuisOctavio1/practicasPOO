f30f00001926fa1ba470e863f3c3d1895d55ab75
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-02-26 07:26:53 -0800

    cambio 9:26
    Finalización de practica

--- practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -1,3 +1,6 @@
+//inicio 7:50
+//fin 9:25
+
 package mx.com.softgame.poo1game.pruebas;
 import mx.com.softgame.poo1game.personajes.Personaje;
 public class PruebaPersonaje {
@@ -7,13 +10,40 @@ public class PruebaPersonaje {
         Personaje per03 = new Personaje("Alan");
         Personaje per04 = new Personaje("Lesly");
         Personaje per05 = new Personaje("Brian");
-
-        for(int i=0;i<3;i++){
+        outfor:
+            for(int i=0;i<3;i++){
+                int ed = ((int)(Math.random()*250));
             
+                if (!per01.setEdad(ed)){
+                    System.out.println(per01.getDetalle() + " sin modificacion en edad a " + ed);
+                }else{
+                    System.out.println(per01.getDetalle() + " se modifico la edad");
+                }
+                ed = ((int)(Math.random()*250));
+                per02.setEdad(ed);
+                if (!per02.setEdad(ed)){
+                    System.out.println(per02.getDetalle() + " sin modificacion en edad a " + ed);
+                }else{
+                    System.out.println(per02.getDetalle() + " se modifico la edad");
+                }
+                per03.setEdad(ed);
+                if (!per03.setEdad(ed)){
+                    System.out.println(per03.getDetalle() + " sin modificacion en edad a " + ed);
+                }else{
+                    System.out.println(per03.getDetalle() + " se modifico la edad");
+                }
+            
+                do {
+                    ed = ((int)(Math.random()*250));
+                    if(ed == 250){
+                        System.out.println("***Bingo***");
+                        continue outfor;
+                    }
+                }while(!per04.setEdad(ed));
+                System.out.println(per04.getDetalle());
+
+
         }
-
-
-
 
     }
     
