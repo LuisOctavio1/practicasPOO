f112fc8766492db84490722d7debca2d556f44a7
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-03-05 06:30:11 -0800

    Cambio 6:29
    Terminando clase planta

--- practica08/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
+++ practica08/src/mx/com/softgame/poo1game/personajes/buenos/Planta.java
@@ -1,4 +1,58 @@
 public class Planta extends Personaje{
+    private char escudo;
+
+    public Planta(String nombre,int vida,char escudo){
+        this.nombre = nombre;
+        this.vida = vida;
+        this.escudo = escudo;
+
+    }
+    public Planta(String nombre,char escudo){
+        this.nombre = nombre;
+        this.vida = 3;
+        this.escudo = escudo;
+
+    }
+    public Planta(String nombre,int vida){
+        this.nombre = nombre;
+        this.vida = vida;
+        this.escudo = 'A';
+
+    }
+    public Planta(String nombre{
+        this.nombre = nombre;
+        this.vida = 3;
+        this.escudo = 'A';
+
+    }
+
+    public char getEscudo(){
+        return escudo;
+    }
+    public String getDetalle(){
+        String detalle = nombre + "\t" + vida + escudo;
+        return detalle;
+
+    }
+    public void  decVida(){
+        int multiplo = 1;
+        if(escudo == 'A'){
+            multiplo = 2;
+        }
+        if(this.vida > 0){
+            this.vida = this.vida - (1 * multiplo);
+        }
+
+    }
+    public void  decVida(int decremento){
+        int multiplo = 1;
+        if(escudo == 'A'){
+            multiplo = 2;
+        }
+        if(decremento * multiplo <= this.vida){
+            this.vida -= decremento;
+        }
+    }
     
     
 }
