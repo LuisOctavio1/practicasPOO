416c44efd918630db5590cbd77a25f603a204e7b
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-03-26 07:51:12 -0700

    Cambio 5
    A mitad del paso 5 7:50

---
+++ practica11/src/mx/com/softgame/poo1game/utils/Tablero.java
@@ -0,0 +1,16 @@
+package mx.com.softgame.poo1game.utils;
+public class Tablero {
+    final static int MAX_LINE = 10;
+    private static int idx = -1;
+    private static Personaje [] personajes = new Personaje[MAX_LINE];
+    
+    private Tablero(){
+    }
+    public static boolean addPersonaje(Personaje p){
+        if(personajes = new Personaje[idx]){
+            return true;
+        }
+        return false;
+    }
+    
+}
