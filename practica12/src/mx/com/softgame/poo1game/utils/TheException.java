29b8aa2ae454f11183264f2de7faa9013d74530a
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-04-16 06:08:18 -0700

    Cambio 2
    Paso3 a las 8:08

--- practica12/src/mx/com/softgame/poo1game/utils/TheException.java
+++ practica12/src/mx/com/softgame/poo1game/utils/TheException.java
@@ -1,5 +1,15 @@
 package mx.com.softgame.poo1game.utils;
 public class TheException extends Exception{
-    int poss;
+    private int poss;
+    
+    public TheException(String msg, int poss){
+        super(msg);
+        this.poss = poss;
+
+    }
+
+    public int getPoss(){
+        return poss;
+    }
 
 }
