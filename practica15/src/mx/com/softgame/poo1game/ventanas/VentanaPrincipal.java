5305959c0c4556a60a77d768cc39814403953255
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-05-07 08:56:40 -0700

    Cambio6
    Empezando el cambio8 10:55 (tuve unos problemas y tuve que dejar de realizar la practica por un tiempo, por eso el tiempo algo largo entre este comit y el anterior)

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -3,13 +3,13 @@ package mx.com.softgame.poo1game.ventanas;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
+
 
 public class VentanaPrincipal{
     JFrame f;
@@ -28,7 +28,7 @@ public class VentanaPrincipal{
         txtCont = new JTextArea(30,40);
         lblLeidos = new JLabel("Caracteres leidos");
         lblN = new JLabel();
-        btnExit = new JButton();
+        btnExit = new JButton("salir");
     }
 
     public void initComponents(){
@@ -36,16 +36,27 @@ public class VentanaPrincipal{
         f.add(lblFile);
         f.add(txtFile);
         f.add(btnOpen);
+        btnOpen.addActionListener(new  ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                e.openFile();
+
+            }
+
+        }
+        );
         f.add(txtCont);
         f.add(lblLeidos);
         f.add(lblN);
         f.add(btnExit);
-        f.setSize(550,440);
-        f.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-              System.exit(0);
+        btnExit.addActionListener(new  ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                f.dispose();
+
             }
-          });
+
+        });
+        f.setSize(550,600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
 
