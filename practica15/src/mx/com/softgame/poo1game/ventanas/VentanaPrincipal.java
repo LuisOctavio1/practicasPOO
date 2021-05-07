3c7ca990e49d9fb1f58e1b8a37a1bfda73e0739b
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-05-07 06:38:43 -0700

    Cambio 3
    Corriegiendo errores del metono intComponents a las 8:38

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,26 +1,49 @@
 package mx.com.softgame.poo1game.ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.FlowLayout;
+
 public class VentanaPrincipal{
-    jFrame f;
-    jLabel lblFile;
-    jTextField txtFile;
-    jButton btnOpen;
-    jTextArea txtCont;
-    jLabel lblLeidos;
-    jLabel lblN;
-    jButton btnExit;
+    JFrame f;
+    JLabel lblFile;
+    JTextField txtFile;
+    JButton btnOpen;
+    JTextArea txtCont;
+    JLabel lblLeidos;
+    JLabel lblN;
+    JButton btnExit;
     public VentanaPrincipal(){
-        f = new jFrame("Practica14");
-        lblFile = new jLabel("Nombre del archivo");
-        txtFile = new jTextField();
-        btnOpen = new jButton("Abrir archivo");
-        txtContenido = new jTextArea(30,40);
-        lblLeidos = new jLabel("Caracteres leidos");
-        lblN = new jLabel(0);
-        btnExit = new jButton();
+        f = new JFrame("Practica14");
+        lblFile = new JLabel("Nombre del archivo");
+        txtFile = new JTextField();
+        btnOpen = new JButton("Abrir archivo");
+        txtCont = new JTextArea(30,40);
+        lblLeidos = new JLabel("Caracteres leidos");
+        lblN = new JLabel();
+        btnExit = new JButton();
     }
 
     public void initComponents(){
-        f.
+        f.setLayout(new FlowLayout());
+        f.add(lblFile);
+        f.add(txtFile);
+        f.add(btnOpen);
+        f.add(txtCont);
+        f.add(lblLeidos);
+        f.add(lblN);
+        f.add(btnExit);
+        f.setSize(550,440);
+        f.action(windowClosing(), System.exit( 0 ));
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        VentanaPrincipal =v = new VentanaPrincipal();
+        v.launchFrame();
     }
 
 
