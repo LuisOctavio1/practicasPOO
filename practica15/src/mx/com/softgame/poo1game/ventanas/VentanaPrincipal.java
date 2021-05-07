3e73d7f2faab080b4914b41d422cb3111775b28b
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-05-07 06:20:55 -0700

    Cambio3
    Creando el metodo initComponents a las 8:20

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -2,13 +2,25 @@ package mx.com.softgame.poo1game.ventanas;
 public class VentanaPrincipal{
     jFrame f;
     jLabel lblFile;
-    jTextField txtFil;
+    jTextField txtFile;
     jButton btnOpen;
     jTextArea txtCont;
     jLabel lblLeidos;
-    jButton btnExis;
+    jLabel lblN;
+    jButton btnExit;
     public VentanaPrincipal(){
         f = new jFrame("Practica14");
+        lblFile = new jLabel("Nombre del archivo");
+        txtFile = new jTextField();
+        btnOpen = new jButton("Abrir archivo");
+        txtContenido = new jTextArea(30,40);
+        lblLeidos = new jLabel("Caracteres leidos");
+        lblN = new jLabel(0);
+        btnExit = new jButton();
+    }
+
+    public void initComponents(){
+        f.
     }
 
 
