ec86626b107821b753775048830ac8bbf09dfdc2
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-05-07 06:05:30 -0700

    Cambio2
    Definiendo el constructor a las 8:05

--- practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,4 +1,15 @@
-package mx.com.softgame.poo1game.vnetanas;
+package mx.com.softgame.poo1game.ventanas;
 public class VentanaPrincipal{
-    
+    jFrame f;
+    jLabel lblFile;
+    jTextField txtFil;
+    jButton btnOpen;
+    jTextArea txtCont;
+    jLabel lblLeidos;
+    jButton btnExis;
+    public VentanaPrincipal(){
+        f = new jFrame("Practica14");
+    }
+
+
 }
