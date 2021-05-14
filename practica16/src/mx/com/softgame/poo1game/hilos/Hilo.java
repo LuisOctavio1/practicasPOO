f35930dc02f4e3fcf8e860436f3d130b4d84d37c
Tavi-xd <79207157+Tavi-xd@users.noreply.github.com>
2021-05-14 06:16:50 -0700

    Cambio 3
    Creando la clase ventanaHilos a las 8:16

--- practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
+++ practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
@@ -1,1 +1,28 @@
 package mx.com.softgame.poo1game.hilos;
+import java.util.Random;
+import javax.swing.JLabel;
+
+public class Hilo extends Thread{
+    JLabel etiqueta;
+    String name;
+
+    public Hilo(String name,JLabel etiqueta){
+        name = this.name;
+        etiqueta = this.etiqueta;
+
+    }
+
+    public void run(){
+        String str;
+        for(int i = 0; i<10;i++){
+            str = etiqueta;
+            char c = charAt(str.length()-1);
+            str += c;
+            etiqueta = str;
+            int n = Math.random()*1001;
+            hilo.sleep(n); 
+        }
+        str += "HECHO";
+        etiqueta = str;
+    }
+}
